30ae79b8f9dcc2a2e0671eedcc8ad23ff4547ef8
htdanilgitvps <168799594+htdanilgitvps@users.noreply.github.com>
2024-06-17 19:51:13 +0545

    Update exec.bat

--- exec.bat
+++ exec.bat
@@ -5,6 +5,6 @@ msiexec /i ChromeRemoteDesktopHost.msi /quiet /norestart
 
 set PIN=123456
 ::go to https://remotedesktop.google.com/headless, copy the Windows (Cmd) code and replace the following code
-set AUTH_CODE="%PROGRAMFILES(X86)%\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0ATx3LY7CWdLkjV624ooJsDC4yTX1E-iHDUNJE5CmsyExX10S_bpBWYOABsM6b-V1QZTwoA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=%COMPUTERNAME%
+set AUTH_CODE="%PROGRAMFILES(X86)%\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0ATx3LY6_im7nO_O244Woms5dHcTcwUgWwxOw74HPlavDLUf-QXX4y8vRQVhezIyjWro11A" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=%COMPUTERNAME%
 
 %AUTH_CODE% --pin=%PIN%
